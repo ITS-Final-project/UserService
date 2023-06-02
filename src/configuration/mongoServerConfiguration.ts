635f008d5feeae4412159790a5377c562c625d70
Andrej Tasevski Mestrovic <akitasevski112@gmail.com>
2023-06-02 23:46:00 +0200

    Updated MongoDB Config

--- src/configuration/mongoServerConfiguration.ts
+++ src/configuration/mongoServerConfiguration.ts
@@ -3,8 +3,14 @@ import dotenv from 'dotenv';
 dotenv.config({path:'.env'});
 
 export class MongoServerConfiguration {
-    HOST = process.env.MONGO_HOST || 'mongodb://localhost';
-    PORT = process.env.MONGO_PORT || 27017;
-    DB = process.env.MONGO_DB || 'zr_users';
+    public static HOST = process.env.DB_HOST || 'localhost';
+    public static PORT = process.env.DB_PORT || 27017;
+    public static DATABASE = process.env.DB_NAME || 'zr_users';
+    public static USER = process.env.READERWRITER_USERNAME
+    public static PASSWORD = process.env.READERWRITER_PASSWORD
+    public static CONNECTION_STRING = "mongodb://" 
+        + MongoServerConfiguration.USER + ":" + MongoServerConfiguration.PASSWORD 
+        + "@" + MongoServerConfiguration.HOST + ":" + MongoServerConfiguration.PORT 
+        + "/" + MongoServerConfiguration.DATABASE;
 }
     
