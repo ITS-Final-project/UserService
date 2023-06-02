b43eb8ed5302539b7c75b035a09620ecbdbbb0c1
Andrej Tasevski Mestrovic <akitasevski112@gmail.com>
2023-06-02 21:56:58 +0200

    HTTP Server init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
 import express from 'express';
 
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
+
+// Import configuration read from env
+import { HttpConfiguration } from './configuration/httpServerConfiguration';
+
+const app = express();
+const port = HttpConfiguration.PORT;
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+    console.log(`User service listening on port ${port}`);
+    }
+);
