104b6922fefdef1de54cbbc528fd76f9a785ebe3
Andrej Tasevski Mestrovic <akitasevski112@gmail.com>
2023-07-16 15:34:51 +0200

    Updated monogDB creds

--- src/configuration/mongoServerConfiguration.ts
+++ src/configuration/mongoServerConfiguration.ts
@@ -8,9 +8,14 @@ export class MongoServerConfiguration {
     public static DATABASE = process.env.DB_NAME || 'zr_users';
     public static USER = process.env.READERWRITER_USERNAME
     public static PASSWORD = process.env.READERWRITER_PASSWORD
-    public static CONNECTION_STRING = "mongodb://" 
+    private static USE_CREDS = process.env.USE_CREDS || false;
+    public static CONNECTION_STRING = 
+        MongoServerConfiguration.USE_CREDS ?
+        "mongodb://" 
         + MongoServerConfiguration.USER + ":" + MongoServerConfiguration.PASSWORD 
         + "@" + MongoServerConfiguration.HOST + ":" + MongoServerConfiguration.PORT 
-        + "/" + MongoServerConfiguration.DATABASE;
+        + "/" + MongoServerConfiguration.DATABASE
+        :
+        "mongodb://" + MongoServerConfiguration.HOST + ":" + MongoServerConfiguration.PORT
 }
     
